c830cd76a965cfec40a503469d13ef1af8d0b0ed
gcmb105 <105737570+gcmb105@users.noreply.github.com>
2023-09-28 23:50:34 -0500

    Delete src directory

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import Boton from 'react-bootstrap/Button';
-import './page.modul.css'
-const Button = ({ onClick, texto, variant, type, className }) => {
-    return (
-        <Boton className={className} onClick={() => onClick } variant={variant} type={type}>{ texto }</Boton>
-    )
-}
-
-export default Button
