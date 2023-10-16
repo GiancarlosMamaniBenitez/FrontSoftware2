d731d41c49849962974609c144fbdaee88346741
CarlosLucano <130522336+CarlosLucano@users.noreply.github.com>
2023-10-15 22:49:49 -0500

    Update page.js
    carlos 15/10/23 - 22:49

--- src/app/DatosPerfil/page.js
+++ src/app/DatosPerfil/page.js
@@ -1,22 +1,14 @@
 'use client'
 
-import './perfil1.css';
-import ProgressBar from '../../components/ProgressBar.jsx';
-import Range from '../../components/Range.jsx';
-import { useState } from 'react';
-import MenuNuevo from '../MenuNuevo';
+import { Tab } from "bootstrap"
+import { useState } from "react"
+import ".//perfil1.css"
+import Form from 'react-bootstrap/Form'
 
-function Info() {
 
-   return <div>
-    <div className="col-2">
-                {/* Agregamos el VerticalMenu */}
-                
-                <MenuNuevo>
-                    
-                </MenuNuevo>
-        </div>
-        <table className="tablaInfo">           
+function Info(){
+    return <div>
+        <table className="tablaInfo">
             <tr>
                 <td><h1 style={{fontSize:30, marginTop:30}}>Mi Perfil</h1></td>
                 <td><button class="cancelar">Cancelar</button></td>
@@ -24,78 +16,71 @@ function Info() {
             </tr>
         </table>
         <hr></hr>
-        <br/><br/>
         <h2 >Informacion Personal</h2>
         <table>
             <tr>
                 <td style={{width: "20%"}}><div className="Contenedor"><label class="label">Nombre</label><br/><input type="text" class="texto" name="" id="" /></div></td>
                 <td style={{width: "20%"}}><div className="Contenedor"><label class="label">Apellidos</label><br/><input type="text" class="texto" name="" id=""/></div></td>
-                <td style={{width: "20%"}}><div className="Contenedor"><label class="label">DNI</label><br/><input type="text" class="texto" name="" id=""/></div></td>
-                <td style={{width: "20%"}}><img src="https://img.freepik.com/premium-photo/blue-circle-with-man-s-head-circle-with-white-background_745528-3499.jpg" style={{ width: "50%" }} alt=""/></td></tr>
+                <td style={{width: "20%"}}><div className="Contenedor"><label class="label">Tipo de Documento</label><br/><input type="text" class="texto" name="" id=""/></div></td>
+                <td style={{width: "20%"}}><img src="https://media.tenor.com/vjDSipHhsjAAAAAd/fortnite-snake-eyes.gif" style={{ width: "50%" }} alt=""/></td></tr>
             
             <tr>
-            <td><div className="Contenedor"><label class="label">Correo</label><br/><input type="text" class="texto" name="" id=""/></div></td>
+            <td><div className="Contenedor"><label class="label">Rol</label><br/><input type="text" class="texto" name="" id=""/></div></td>
                 <td></td>
-                
                 <td><div className="Contenedor"><label class="label">Numero</label><br/><input type="text" class="texto" name="" id=""/></div></td>
                 <td>Adjuntar foto</td>
             </tr>
         </table>
-        <br/><br/>
-   </div>
+    </div>
 }
 
-
-
 function Datos(){
-
     const [opt, setOpt] = useState(0)
-
+    
     let x
     if (opt === 0){
         x = <table className="tablaDatos"><br/>
         <tr>
-            <td><div className="Contenedor"><br/><input type="text" class="texto3" name="" id=""/><label class="label3">Pais</label></div></td>
+            <td><div className="Contenedor"><br/><input type="text" class="texto3" name="" id=""/><label class="label3">Usuario</label></div></td>
         </tr>
         <tr>
-            <td><div className="Contenedor"><label class="label3">Ciudad</label><br/><input type="password" class="texto3" name="" id=""/></div></td>
-            <td><div className="Contenedor"><label class="label3">Dirección</label><br/><input type="password" class="texto3" name="" id="" /></div></td>
-            <td><div className="Contenedor"><label class="label3">Codigo Postal</label><br/><input type="password" class="texto3" name="" id="" /></div></td>
+            <td><div className="Contenedor"><label class="label3">Contraseña Actual</label><br/><input type="password" class="texto3" name="" id=""/></div></td>
+            <td><div className="Contenedor"><label class="label3">Nueva Contraseña</label><br/><input type="password" class="texto3" name="" id="" /></div></td>
+            <td><div className="Contenedor"><label class="label3">Repetir Contraseña</label><br/><input type="password" class="texto3" name="" id="" /></div></td>
         </tr>
         </table>
     }
     else if(opt === 1){
         x = <table className="tablaDatos"><br/>
         <tr>
-            <td><div className="Contenedor"><label htmlFor="" class="label3" type="text" >Estudiante o trabajador</label><br/><input type="text" class="texto3"/></div></td>
+            <td><div className="Contenedor"><label htmlFor="" class="label3" type="text" >Universidad</label><br/><input type="text" class="texto3"/></div></td>
            
     
-            <td><div className="Contenedor"><label htmlFor="" class="label3">Te encuentras trabajando actualmente?</label><br/><input type="text" class="texto3" name="" id=""/></div></td>
+            <td><div className="Contenedor"><label htmlFor="" class="label3">Agregar cursos</label><br/><input type="text" class="texto3" name="" id=""/></div></td>
         </tr>
         <tr>
-            <td><div className="Contenedor"><label class="label3">¿Cuanto es tu ingreso mensual?</label><br/><input type="text" class="texto3" name=""/></div></td>
+            <td><div className="Contenedor"><label class="label3">Carrera</label><br/><input type="text" class="texto3" name=""/></div></td>
         </tr>   
         </table>
     }
     else{
         x = <table className="tablaDatos"><br/>
-            <tr><div className="Contenedor"><label class="label2">Empresa de trabajo</label><br/><input type="text" class="texto2" name=""/></div></tr>
-            <tr><div className="Contenedor"><label class="label2">Cuanto tiempo llevas ahi</label><br/><input type="text" class="texto2" name=""/></div></tr>
+            <tr><div className="Contenedor"><label class="label2">Titulo</label><br/><input type="text" class="texto2" name=""/></div></tr>
+            <tr><div className="Contenedor"><label class="label2">Descripcion</label><br/><input type="text" class="texto2" name=""/></div></tr>
             </table>
     }
-    return <div><button className="datos" onClick={()=>setOpt(0)}>Lugar donde vive</button><button className="datos" onClick={()=>setOpt(1)}>Situación Actual</button><button className="datos" onClick={()=>setOpt(2)}>Lugar de trabajo</button>
+    return <div><button className="datos" onClick={()=>setOpt(0)}>Datos de usuario</button><button className="datos" onClick={()=>setOpt(1)}>Universidad</button><button className="datos" onClick={()=>setOpt(2)}>Presentacion</button>
     {x}
     </div>
 }
 
-function DatosdePerfil(){
+function PerfilDocente() {
     return <div>
-        <div className = "row">
-            <div className = "col-md-11"><Info/>
-            </div>
-                <Datos/>
-        </div>
+        <div className="row">
+        <div className="col-md-11"><Info/>
+        <Datos/></div>
     </div>
-    }
+    </div>
+}
 
-export default DatosdePerfil
+export default PerfilDocente
