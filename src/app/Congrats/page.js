2324e19ac6c814073a67ccc9ea2e83dfaecc7920
Franco Sotelo <89244480+francosotelogomez@users.noreply.github.com>
2023-11-08 18:08:41 -0500

    Update page.js

--- src/app/Congrats/page.js
+++ src/app/Congrats/page.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import "./congrats.css";
 const Congratulations = () => {
     return (
-        <div className="container">
+        <div className="container-congrats">
             <h1>Congratulations!</h1><br/>
             <p>You have successfully signed up for.</p>
             <img src="https://cdn.discordapp.com/attachments/1025977476096204862/1162442035278659604/logo-name.jpeg?ex=6557a302&is=65452e02&hm=ed32a4110d4de5892a644263daa46d8d40dfe76039a8a03405ccbfbd00063801&" alt="" />
